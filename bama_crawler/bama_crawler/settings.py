9332b44155208742cea48c0b5a3b1e528abdc0f9
Alirezaalireza <alireza.hashemian77@gmail.com>
2024-10-08 09:51:52 +0330

    using selenium

--- bama_crawler/bama_crawler/settings.py
+++ bama_crawler/bama_crawler/settings.py
@@ -91,3 +91,18 @@ ROBOTSTXT_OBEY = True
 REQUEST_FINGERPRINTER_IMPLEMENTATION = "2.7"
 TWISTED_REACTOR = "twisted.internet.asyncioreactor.AsyncioSelectorReactor"
 FEED_EXPORT_ENCODING = "utf-8"
+
+
+SPLASH_URL = 'http://localhost:8050'
+
+DOWNLOADER_MIDDLEWARES = {
+    'scrapy_splash.SplashCookiesMiddleware': 723,
+    'scrapy_splash.SplashMiddleware': 725,
+    'scrapy.downloadermiddlewares.httpcompression.HttpCompressionMiddleware': 810,
+}
+
+SPIDER_MIDDLEWARES = {
+    'scrapy_splash.SplashDeduplicateArgsMiddleware': 100,
+}
+
+DUPEFILTER_CLASS = 'scrapy_splash.SplashAwareDupeFilter'
--- bama_crawler/bama_crawler/spiders/bama_car.py
+++ bama_crawler/bama_crawler/spiders/bama_car.py
@@ -1,81 +1,63 @@
 import scrapy
-from scrapy.linkextractors import LinkExtractor
-from scrapy.spiders import CrawlSpider, Rule
+import time
+import tempfile
+from scrapy.selector import Selector
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.options import Options
 
 
-class CarSpider(CrawlSpider):
+class CarSpider(scrapy.Spider):
     name = 'bama'
     allowed_domains = ['bama.ir']
-    start_urls = ['https://www.bama.ir/car']
+    start_urls = [
+        'https://www.bama.ir/car?page=1',
+    ]
 
-    rules = (
-        Rule(LinkExtractor(restrict_xpaths='//div[@class="bama-adlist-container"]//a'), callback='parse_pricing',
-             follow=False),
-        Rule(LinkExtractor(restrict_xpaths='//a[@class="bama-ad listing"]/href'), follow=True),
-    )
+    def __init__(self, *args, **kwargs):
+        super(CarSpider, self).__init__(*args, **kwargs)
+        options = Options()
+        options.headless = True
 
-    def parse_pricing(self, response):
+        options.add_argument('--no-sandbox')  # Required for running as root
+        options.add_argument('--disable-dev-shm-usage')  # Overcome limited resource issues
+        options.add_argument('--disable-gpu')  # Disable GPU for headless mode
+        options.add_argument('--remote-debugging-port=9222')  # Enable remote debugging
 
+        profile_dir = tempfile.mkdtemp()
+        options.add_argument(f"user-data-dir={profile_dir}")
+
+        self.driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=options)
+
+    def parse(self, response):
+        self.driver.get(response.url)
+
+        last_height = self.driver.execute_script("return document.body.scrollHeight")
+        while True:
+            self.driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+            time.sleep(2)
+
+            new_height = self.driver.execute_script("return document.body.scrollHeight")
+            if new_height == last_height:
+                break
+            last_height = new_height
+
+        sel = Selector(text=self.driver.page_source)
+        car_links = sel.xpath('//div[@class="bama-adlist-container"]//a/@href').extract()
+
+        for link in car_links:
+            yield response.follow(link, callback=self.parse_car)
+
+        self.driver.quit()
+
+    def parse_car(self, response):
+        # Extract car model and price
         model = response.xpath('normalize-space(//h1[@class="bama-ad-detail-title__title"])').get()
-        price = response.xpath('normalize-space(//div[@class="bama-ad-detail-price__section"]//span[@class="bama-ad-detail-price__price-text"])').get()
+        price = response.xpath(
+            'normalize-space(//div[@class="bama-ad-detail-price__section"]//span[@class="bama-ad-detail-price__price-text"])').get()
 
         yield {
             'model': model,
             'price': price if price else None
         }
-
-
-
-#
-# import scrapy
-# from scrapy.selector import Selector
-# from scrapy.crawler import CrawlerProcess
-# from selenium import webdriver
-# from selenium.webdriver.firefox.service import Service as FirefoxService
-# from webdriver_manager.firefox import GeckoDriverManager
-# import tempfile
-#
-# class CarSpider(scrapy.Spider):
-#     name = 'bama'
-#     allowed_domains = ['bama.ir']
-#     start_urls = ['https://www.bama.ir/car']
-#
-#     def __init__(self, *args, **kwargs):
-#         super(CarSpider, self).__init__(*args, **kwargs)
-#         options = webdriver.FirefoxOptions()
-#         options.headless = True  # Run in headless mode
-#
-#         profile_dir = tempfile.mkdtemp()
-#         options.profile = profile_dir
-#
-#         self.driver = webdriver.Firefox(service=FirefoxService(GeckoDriverManager().install()), options=options)
-#
-#     def parse(self, response):
-#         self.driver.get(response.url)
-#
-#         last_height = self.driver.execute_script("return document.body.scrollHeight")
-#         while True:
-#             self.driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-#             time.sleep(2)
-#
-#             new_height = self.driver.execute_script("return document.body.scrollHeight")
-#             if new_height == last_height:
-#                 break
-#             last_height = new_height
-#
-#         sel = Selector(text=self.driver.page_source)
-#         car_links = sel.xpath('//div[@class="bama-adlist-container"]//a/@href').extract()
-#
-#         for link in car_links:
-#             yield response.follow(link, callback=self.parse_car)
-#
-#         self.driver.quit()
-#
-#     def parse_car(self, response):
-#         model = response.xpath('normalize-space(//h1[@class="bama-ad-detail-title__title"])').get()
-#         price = response.xpath('normalize-space(//div[@class="bama-ad-detail-price__section"]//span[@class="bama-ad-detail-price__price-text"])').get()
-#
-#         yield {
-#             'model': model,
-#             'price': price if price else None
-#         }

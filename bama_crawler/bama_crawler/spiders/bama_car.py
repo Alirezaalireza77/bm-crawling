ea9f13bd4c362dfc68db4c4ed36e7b91c8c13211
Alirezaalireza <alireza.hashemian77@gmail.com>
2024-10-08 14:31:52 +0330

    pagination

--- bama_crawler/bama_crawler/spiders/bama_car.py
+++ bama_crawler/bama_crawler/spiders/bama_car.py
@@ -1,3 +1,72 @@
+# import scrapy
+# import time
+# import tempfile
+# from scrapy.selector import Selector
+# from selenium import webdriver
+# from selenium.webdriver.chrome.service import Service as ChromeService
+# from webdriver_manager.chrome import ChromeDriverManager
+# from selenium.webdriver.chrome.options import Options
+#
+#
+# class CarSpider(scrapy.Spider):
+#     name = 'bama'
+#     allowed_domains = ['bama.ir']
+#     start_urls = [
+#         'https://www.bama.ir/car?page=1'
+#     ]
+#
+#     def __init__(self, *args, **kwargs):
+#         super(CarSpider, self).__init__(*args, **kwargs)
+#         options = Options()
+#         options.headless = True
+#
+#         options.add_argument('--no-sandbox')
+#         options.add_argument('--disable-dev-shm-usage')
+#         options.add_argument('--disable-gpu')
+#         options.add_argument('--remote-debugging-port=9222')
+#
+#         profile_dir = tempfile.mkdtemp()
+#         options.add_argument(f"user-data-dir={profile_dir}")
+#
+#         self.driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=options)
+#
+#     def parse(self, response):
+#         self.driver.get(response.url)
+#
+#         last_height = self.driver.execute_script("return document.body.scrollHeight")
+#         while True:
+#             self.driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+#             time.sleep(2)
+#
+#             new_height = self.driver.execute_script("return document.body.scrollHeight")
+#             if new_height == last_height:
+#                 break
+#             last_height = new_height
+#
+#         sel = Selector(text=self.driver.page_source)
+#         car_links = sel.xpath('//div[@class="bama-adlist-container"]//a/@href').extract()
+#
+#         for link in car_links:
+#             yield response.follow(link, callback=self.parse_car)
+#
+#         self.driver.quit()
+#
+#     def parse_car(self, response):
+#
+#         model = response.xpath('normalize-space(//h1[@class="bama-ad-detail-title__title"])').get()
+#         price = response.xpath(
+#             'normalize-space(//div[@class="bama-ad-detail-price__section"]//span[@class="bama-ad-detail-price__price-text"])').get()
+#
+#         yield {
+#             'model': model,
+#             'price': price if price else None
+#         }
+#
+#
+#
+#
+
+
 import scrapy
 import time
 import tempfile
@@ -12,14 +81,13 @@ class CarSpider(scrapy.Spider):
     name = 'bama'
     allowed_domains = ['bama.ir']
     start_urls = [
-        'https://www.bama.ir/car?page=1',
+        'https://www.bama.ir/car?page=1'
     ]
 
     def __init__(self, *args, **kwargs):
         super(CarSpider, self).__init__(*args, **kwargs)
         options = Options()
         options.headless = True
-
         options.add_argument('--no-sandbox')
         options.add_argument('--disable-dev-shm-usage')
         options.add_argument('--disable-gpu')
@@ -33,26 +101,31 @@ class CarSpider(scrapy.Spider):
     def parse(self, response):
         self.driver.get(response.url)
 
+        # Scroll to load the page
         last_height = self.driver.execute_script("return document.body.scrollHeight")
         while True:
             self.driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
             time.sleep(2)
-
             new_height = self.driver.execute_script("return document.body.scrollHeight")
             if new_height == last_height:
                 break
             last_height = new_height
 
+        # Scraping the data from the current page
         sel = Selector(text=self.driver.page_source)
         car_links = sel.xpath('//div[@class="bama-adlist-container"]//a/@href').extract()
 
         for link in car_links:
             yield response.follow(link, callback=self.parse_car)
 
-        self.driver.quit()
+        # Handle pagination - Check if the next page exists
+        next_page = sel.xpath('//a[@class="next"]/@href').get()  # Update the XPath according to the site's next button
+        if next_page:
+            next_page_url = response.urljoin(next_page)
+            # Scrapy's way of handling multiple pages
+            yield scrapy.Request(url=next_page_url, callback=self.parse)
 
     def parse_car(self, response):
-
         model = response.xpath('normalize-space(//h1[@class="bama-ad-detail-title__title"])').get()
         price = response.xpath(
             'normalize-space(//div[@class="bama-ad-detail-price__section"]//span[@class="bama-ad-detail-price__price-text"])').get()
@@ -61,7 +134,3 @@ class CarSpider(scrapy.Spider):
             'model': model,
             'price': price if price else None
         }
-
-
-
-

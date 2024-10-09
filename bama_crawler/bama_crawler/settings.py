92243796c2f62894c87542ac188669d1bb770b61
Alirezaalireza <alireza.hashemian77@gmail.com>
2024-10-09 10:58:46 +0330

    correctly work

--- bama_crawler/bama_crawler/settings.py
+++ bama_crawler/bama_crawler/settings.py
@@ -17,7 +17,7 @@ NEWSPIDER_MODULE = "bama_crawler.spiders"
 #USER_AGENT = "bama_crawler (+http://www.yourdomain.com)"
 
 # Obey robots.txt rules
-ROBOTSTXT_OBEY = True
+ROBOTSTXT_OBEY = False
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
 #CONCURRENT_REQUESTS = 32
@@ -95,14 +95,28 @@ FEED_EXPORT_ENCODING = "utf-8"
 
 SPLASH_URL = 'http://localhost:8050'
 
-DOWNLOADER_MIDDLEWARES = {
-    'scrapy_splash.SplashCookiesMiddleware': 723,
-    'scrapy_splash.SplashMiddleware': 725,
-    'scrapy.downloadermiddlewares.httpcompression.HttpCompressionMiddleware': 810,
-}
+# DOWNLOADER_MIDDLEWARES = {
+#     'scrapy_splash.SplashCookiesMiddleware': 723,
+#     'scrapy_splash.SplashMiddleware': 725,
+#     'scrapy.downloadermiddlewares.httpcompression.HttpCompressionMiddleware': 810,
+# }
 
-SPIDER_MIDDLEWARES = {
-    'scrapy_splash.SplashDeduplicateArgsMiddleware': 100,
-}
+# SPIDER_MIDDLEWARES = {
+#     'scrapy_splash.SplashDeduplicateArgsMiddleware': 100,
+# }
 
 DUPEFILTER_CLASS = 'scrapy_splash.SplashAwareDupeFilter'
+
+
+# Enable Selenium
+DOWNLOADER_MIDDLEWARES = {
+    'scrapy_selenium.SeleniumMiddleware': 800,
+    'scrapy.downloadermiddlewares.useragent.UserAgentMiddleware': None,
+}
+
+# SELENIUM_DRIVER_NAME = 'chrome'
+# SELENIUM_DRIVER_EXECUTABLE_PATH = '/path/to/chromedriver'
+# SELENIUM_DRIVER_ARGUMENTS = ['--headless']  # Use headless mode if needed
+
+
+
--- bama_crawler/bama_crawler/spiders/bama_car.py
+++ bama_crawler/bama_crawler/spiders/bama_car.py
@@ -1,72 +1,3 @@
-# import scrapy
-# import time
-# import tempfile
-# from scrapy.selector import Selector
-# from selenium import webdriver
-# from selenium.webdriver.chrome.service import Service as ChromeService
-# from webdriver_manager.chrome import ChromeDriverManager
-# from selenium.webdriver.chrome.options import Options
-#
-#
-# class CarSpider(scrapy.Spider):
-#     name = 'bama'
-#     allowed_domains = ['bama.ir']
-#     start_urls = [
-#         'https://www.bama.ir/car?page=1'
-#     ]
-#
-#     def __init__(self, *args, **kwargs):
-#         super(CarSpider, self).__init__(*args, **kwargs)
-#         options = Options()
-#         options.headless = True
-#
-#         options.add_argument('--no-sandbox')
-#         options.add_argument('--disable-dev-shm-usage')
-#         options.add_argument('--disable-gpu')
-#         options.add_argument('--remote-debugging-port=9222')
-#
-#         profile_dir = tempfile.mkdtemp()
-#         options.add_argument(f"user-data-dir={profile_dir}")
-#
-#         self.driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=options)
-#
-#     def parse(self, response):
-#         self.driver.get(response.url)
-#
-#         last_height = self.driver.execute_script("return document.body.scrollHeight")
-#         while True:
-#             self.driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-#             time.sleep(2)
-#
-#             new_height = self.driver.execute_script("return document.body.scrollHeight")
-#             if new_height == last_height:
-#                 break
-#             last_height = new_height
-#
-#         sel = Selector(text=self.driver.page_source)
-#         car_links = sel.xpath('//div[@class="bama-adlist-container"]//a/@href').extract()
-#
-#         for link in car_links:
-#             yield response.follow(link, callback=self.parse_car)
-#
-#         self.driver.quit()
-#
-#     def parse_car(self, response):
-#
-#         model = response.xpath('normalize-space(//h1[@class="bama-ad-detail-title__title"])').get()
-#         price = response.xpath(
-#             'normalize-space(//div[@class="bama-ad-detail-price__section"]//span[@class="bama-ad-detail-price__price-text"])').get()
-#
-#         yield {
-#             'model': model,
-#             'price': price if price else None
-#         }
-#
-#
-#
-#
-
-
 import scrapy
 import time
 import tempfile
@@ -81,7 +12,7 @@ class CarSpider(scrapy.Spider):
     name = 'bama'
     allowed_domains = ['bama.ir']
     start_urls = [
-        'https://www.bama.ir/car?page=1'
+        'https://www.bama.ir/car',
     ]
 
     def __init__(self, *args, **kwargs):
@@ -92,6 +23,9 @@ class CarSpider(scrapy.Spider):
         options.add_argument('--disable-dev-shm-usage')
         options.add_argument('--disable-gpu')
         options.add_argument('--remote-debugging-port=9222')
+        options.add_argument('--window-size=1920,1080')
+        options.add_argument('--disable-blink-features=AutomationControlled')
+        options.add_argument('--disable-software-rasterizer')
 
         profile_dir = tempfile.mkdtemp()
         options.add_argument(f"user-data-dir={profile_dir}")
@@ -101,7 +35,6 @@ class CarSpider(scrapy.Spider):
     def parse(self, response):
         self.driver.get(response.url)
 
-        # Scroll to load the page
         last_height = self.driver.execute_script("return document.body.scrollHeight")
         while True:
             self.driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
@@ -111,18 +44,15 @@ class CarSpider(scrapy.Spider):
                 break
             last_height = new_height
 
-        # Scraping the data from the current page
         sel = Selector(text=self.driver.page_source)
         car_links = sel.xpath('//div[@class="bama-adlist-container"]//a/@href').extract()
 
         for link in car_links:
             yield response.follow(link, callback=self.parse_car)
 
-        # Handle pagination - Check if the next page exists
-        next_page = sel.xpath('//a[@class="next"]/@href').get()  # Update the XPath according to the site's next button
+        next_page = sel.xpath('//a[@class="next"]/@href').get()
         if next_page:
             next_page_url = response.urljoin(next_page)
-            # Scrapy's way of handling multiple pages
             yield scrapy.Request(url=next_page_url, callback=self.parse)
 
     def parse_car(self, response):
@@ -134,3 +64,4 @@ class CarSpider(scrapy.Spider):
             'model': model,
             'price': price if price else None
         }
+
